945835c70fdfd23e080017d8e03555b94df69f03
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2025-03-03 22:33:33 -0500

    Update
    Revamped

--- noteAction.js
+++ noteAction.js
@@ -1,6 +1,26 @@
+// As basic as can be...
 // Create | Read | Update | Delete
 
-// Create
-function AddListItem() {
-    
+// Adds input li to ul on click
+function addNoteToList(){
+    //Get the ul container
+    let ul = document.getElementById('note-container');
+    //Get the user input value
+    let userInputValue = document.getElementById('userInput').value;
+    //Create li for the note
+    let li = document.createElement('li');
+    //Create the edit and delete buttons
+    createEditBtn();
+    createDeleteBtn();
+    //Assign the user input to the li element
+    li.innerHTML = userInputValue;
+    //Append the li element to the ul
+    ul.appendChild(li);
 }
+
+function createEditBtn (){}
+
+function createDeleteBtn(){}
+
+// Deletes input on click
+function clearInput(){}
