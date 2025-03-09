eff281575e01eadb100ee695e95ea5e1606965b9
Jordan Bugglin <62662632+Jbugglin@users.noreply.github.com>
2025-03-09 15:24:26 -0400

    Update
    Will have to figure out how to delete single items and update list. The setup local storage for persistence.

--- noteAction.js
+++ noteAction.js
@@ -1,6 +1,5 @@
 //Create | Read | Update | Delete
 
-//Create
 function submitNote() {
     let task = document.getElementById('user-input').value;
     if (task != ""){
@@ -14,21 +13,33 @@ function submitNote() {
     }
 }
 
-//Read
 //Takes our task, creates text node and appends it to a li element then appends that li element to the ol list. 
 function addNotetoLi(task) {
     //Get our list (ol)
-    let oList = document.getElementById('list-container');
+    let uList = document.getElementById('list-container');
     //Create our li
     let li = document.createElement('li');
     //Append our text to an li element
     li.appendChild(document.createTextNode(task));
+
+    //Add Delete button to the li as well
+    let deleteBtn = document.createElement('input');
+    deleteBtn.setAttribute('type', 'button');
+    deleteBtn.setAttribute('value', 'Delete');
+    deleteBtn.setAttribute('onclick', 'deleteNote()');
+    li.appendChild(deleteBtn);
     //Append our li element to the list
-    oList.appendChild(li);
+    uList.appendChild(li);
 }
 
-//Update -- Need to figure out how to edit a li element
+//Delete -- This will delete all items from the list we created
+function deleteUl() {
+    const ul = document.getElementById('list-container');
+    ul.innerHTML = '';
+}
 
-//Delete -- Need to figure out how to delete a li element
+function deleteNote() {
+    const testList = document.querySelectorAll('li');
 
+}
 //Bonus: utilize localStorage to save...
